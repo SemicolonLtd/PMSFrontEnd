7e6d7b4407e62e1f563e6ba9ff1956c4105138fb
AhmedHamza7 <ahmd7mza@gmail.com>
2024-12-30 11:26:18 +0200

    Handle design

--- src/app/shared/components/project-card/project-card.component.ts
+++ src/app/shared/components/project-card/project-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-project-card',
@@ -7,4 +8,6 @@ import { Component, Input } from '@angular/core';
 })
 export class ProjectCardComponent {
   @Input() projectData:any;
+  lang = environment.lang
+
 }
