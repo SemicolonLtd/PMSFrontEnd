a81c44b7da66a757a598995ad35d08788c3e6cf0
ahmed hassan <ah2300487@gmail.com>
2025-04-27 09:55:02 +0300

    update

--- src/app/modules/complaints/components/get-complaints/get-complaints.component.ts
+++ src/app/modules/complaints/components/get-complaints/get-complaints.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 declare var bootstrap: any;
 
@@ -9,25 +9,29 @@ declare var bootstrap: any;
   styleUrls: ['./get-complaints.component.scss']
 })
 export class GetComplaintsComponent {
-  @Input({required:true}) complaintData:any;
+  @Input({ required: true }) complaintData: any;
 
-  selectedFile: string = '';
+  selectedFile: SafeResourceUrl | string = '';
+
+  constructor(private sanitizer: DomSanitizer) {}
 
   openFileModal(fileUrl: string) {
-    this.selectedFile = fileUrl;
+    if (this.isImage(fileUrl)) {
+      this.selectedFile = fileUrl;
+    } else {
+      this.selectedFile = this.sanitizer.bypassSecurityTrustResourceUrl(fileUrl);
+    }
     const modal = new bootstrap.Modal(document.getElementById('fileModal')!);
     modal.show();
   }
-  
-  isImage(url: string): boolean {
+
+  isImage(url: any): boolean {
     return /\.(jpg|jpeg|png|gif|bmp|webp)$/i.test(url);
   }
 
-
   getRowsCount(message: string): number {
-    if (!message) return 1;  
+    if (!message) return 1;
     const lines = message.split(/\r\n|\n/);
-    return Math.max(lines.length, 3); 
+    return Math.max(lines.length, 3);
   }
-
 }
