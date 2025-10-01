e96f4d95e2a03b9b4f80c52dc59bbcd33ad2bad6
ahmed hassan <ah2300487@gmail.com>
2025-10-01 19:37:28 +0300

    fix header

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     lang:'en',
-    apiUrl: 'https://dashboard.pmsoffshore.com/pms/api',
-    // apiUrl: 'https://pms.gostcode.com/public/pms/api',
+    // apiUrl: 'https://dashboard.pmsoffshore.com/pms/api',
+    apiUrl: 'https://pms.gostcode.com/public/pms/api',
     apiUrl2: 'https://pms.gostcode.com/public/pms/api',
     websiteUrl:'https://pms-frontend.gostcode.com',
     siteKey:'6LfAZZIqAAAAANBhuGiFIX8zSOoD7d1hBohD5ApD'
