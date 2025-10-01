22959f4ba19aca39e54384400443a2bc9d570770
ahmed hassan <ah2300487@gmail.com>
2025-10-01 19:48:02 +0300

    chage baeurl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
     lang:'en',
-    // apiUrl: 'https://dashboard.pmsoffshore.com/pms/api',
-    apiUrl: 'https://pms.gostcode.com/public/pms/api',
-    apiUrl2: 'https://pms.gostcode.com/public/pms/api',
-    websiteUrl:'https://pms-frontend.gostcode.com',
+    apiUrl: 'https://dashboard.pmsoffshore.com/pms/api',
+    // apiUrl: 'https://pms.gostcode.com/public/pms/api',
+    apiUrl2: 'https://dashboard.pmsoffshore.com/pms/api',
+    websiteUrl:'https://pmsoffshore.com',
     siteKey:'6LfAZZIqAAAAANBhuGiFIX8zSOoD7d1hBohD5ApD'
 };
 
